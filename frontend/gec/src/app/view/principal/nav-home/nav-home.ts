da57b955cd706f49e7037593fae8b77b66777f76
JulioMartin12 <rl_yoo@yahoo.com.ar>
2025-08-16 05:07:00 +0000

    creando los componentes compartido y la home

---
+++ frontend/gec/src/app/view/principal/nav-home/nav-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-home',
+  imports: [],
+  templateUrl: './nav-home.html',
+  styleUrl: './nav-home.css'
+})
+export class NavHome {
+
+}
