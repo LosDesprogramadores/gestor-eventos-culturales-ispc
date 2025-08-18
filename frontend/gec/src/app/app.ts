5e706cfd8d02969b1ddc9a57268aba1d122f73b9
JulioMartin12 <rl_yoo@yahoo.com.ar>
2025-08-18 17:53:29 +0000

    Actualizando el json y creando nuevos componentes

--- frontend/gec/src/app/app.ts
+++ frontend/gec/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
