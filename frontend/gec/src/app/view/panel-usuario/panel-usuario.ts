939036762c012af05e55cabb3a97c563aea49298
Juan Ignacio Moreno <126112348+Monky033@users.noreply.github.com>
2025-08-22 14:49:00 -0300

    Update panel-usuario.ts
    se agregan imports

--- frontend/gec/src/app/view/panel-usuario/panel-usuario.ts
+++ frontend/gec/src/app/view/panel-usuario/panel-usuario.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
+import { NavHome } from '../home/nav-home/nav-home';
 
 @Component({
   selector: 'app-panel-usuario',
+  imports: [ Header, Footer, NavHome ],
   templateUrl: './panel-usuario.html',
   styleUrls: ['./panel-usuario.css']
 })
