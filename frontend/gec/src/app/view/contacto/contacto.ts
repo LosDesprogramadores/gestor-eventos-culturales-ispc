6fe76a68747cfb5f10b8197f66272c9f6d8d4418
Juan Ignacio Moreno <126112348+Monky033@users.noreply.github.com>
2025-08-22 14:50:00 -0300

    Update contacto.ts
    se agregan imports

--- frontend/gec/src/app/view/contacto/contacto.ts
+++ frontend/gec/src/app/view/contacto/contacto.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
+import { NavHome } from '../home/nav-home/nav-home';
 
 @Component({
   selector: 'app-contacto',
-  imports: [RouterModule],
+  imports: [RouterModule, Header, Footer, NavHome],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css'
 })
