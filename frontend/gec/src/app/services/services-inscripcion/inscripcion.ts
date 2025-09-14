c1de8a80390f7c88f2ebea07f33f53b8b0dc2c56
monky033 <juanimignacio@gmail.com>
2025-09-14 15:52:55 -0300

    inscripcionesjuani

---
+++ frontend/gec/src/app/services/services-inscripcion/inscripcion.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface Inscripcion {
+  id?: number;             
+  id_usuario: number;
+  id_evento: number;
+  fecha_inscripcion: string;
+  id_estado: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InscripcionService {
+  private apiUrl = `${environment.apiUrl}/inscripciones`;
+
+  constructor(private http: HttpClient) {}
+
+  
+  getInscripciones(): Observable<Inscripcion[]> {
+    return this.http.get<Inscripcion[]>(this.apiUrl);
+  }
+
+  
+  getInscripcionesByUsuario(id_usuario: number): Observable<Inscripcion[]> {
+    return this.http.get<Inscripcion[]>(`${this.apiUrl}?id_usuario=${id_usuario}`);
+  }
+
+  
+  addInscripcion(insc: Inscripcion): Observable<Inscripcion> {
+    return this.http.post<Inscripcion>(this.apiUrl, insc);
+  }
+
+  
+  deleteInscripcion(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
--- frontend/gec/src/environments/environment.ts
+++ frontend/gec/src/environments/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAlsn3CO_KbtnoezSxkjaU7P6hpvu46vuk",
-    authDomain: "basecultura-c7f7a.firebaseapp.com",
-    projectId: "basecultura-c7f7a",
-    storageBucket: "basecultura-c7f7a.firebasestorage.app",
-    messagingSenderId: "386125411449",
-    appId: "1:386125411449:web:d50f988c9ba5bedb49d0a8"
-  }
+  apiUrl: 'http://localhost:3000' 
 };
+
