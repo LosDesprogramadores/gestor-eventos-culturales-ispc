96d84d7532bfa89a87954a16127b7f2d040c2b67
JulioMartin12 <rl_yoo@yahoo.com.ar>
2025-09-27 04:10:15 +0000

    fix:componente panel-usuario

--- frontend/gec/src/app/view/paneles-usuarios/panel-usuario/panel-usuario.ts
+++ frontend/gec/src/app/view/paneles-usuarios/panel-usuario/panel-usuario.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EventoService, Evento } from '../../../services/evento.service';
 import { InscripcionService } from '../../../services/services-inscripcion/inscripcion';
 import { Header } from '../../../shared/header/header';
 import { Footer } from '../../../shared/footer/footer';
@@ -8,6 +7,9 @@ import { NavHome } from '../../home/nav-home/nav-home';
 import { Auth } from '../../../services/service-autenticacion/auth.service';
 import { ClassEvento } from '../../../model/evento';
 import { Inscripcion } from '../../../model/inscripcion';
+import { Observable} from 'rxjs';
+import { SAlert } from '../../../services/service-alert/s-alert';
+import { Mensaje } from '../../../model/mensaje';
 
 @Component({
   selector: 'app-panel-usuario',
@@ -17,90 +19,105 @@ import { Inscripcion } from '../../../model/inscripcion';
   styleUrl: './panel-usuario.css'
 })
 export class PanelUsuarioComponent implements OnInit {
+
+editarEvento(_t21: ClassEvento) {
+throw new Error('Method not implemented.');
+}
   recomendados: ClassEvento[] = [];
   agendados: ClassEvento[] = [];
-  inscripciones : Inscripcion [] = [];
+  inscripciones$ ! : Observable<Inscripcion []>;
+  eventos$ ! : Observable<ClassEvento []>;
+  hoverVisible = true;
+  mensaje = new Mensaje();
+  mostrarModalEliminar = false
+  inscripcionEliminar: Inscripcion | null = null; 
 
   constructor(
-    private eventoService: EventoService,
-    private inscripcionService: InscripcionService,
-    private auth: Auth
+      private inscripcionService: InscripcionService,
+    private auth: Auth, private mensajeAlert :SAlert 
   ) { }
 
   ngOnInit(): void {
-    //this.cargarRecomendados();
-    this.cargarAgendados();
+    this.mensajeAlert.alert$.subscribe((res:Mensaje)=> {
+    this.mensaje.$showMessage = res.$showMessage;
+    this.mensaje.$message = res.$message;
+    this.mensaje.$tipoAlerta = res.$tipoAlerta;
+    const tiempo = res.$time ?? 2000;
+ 
+   setTimeout(()=>{ 
+     this.mensaje.$showMessage = false;
+    }, 
+     tiempo)
+      });
+    this.cargarMisInscriciones();
   }
 
-
- /*  cargarRecomendados(): void {
-    this.eventoService.getEventos().subscribe({
-      next: (data) => (this.recomendados = data.slice(0, 4)),
-      error: (err) => console.error('Error cargando eventos recomendados', err)
-    });
-  } */
-
-
-  cargarAgendados(): void {
-    const uid = this.auth.usuarioLogueadoId();
-    if (!uid) return;
-
-    this.inscripcionService.getInscripcionesByUsuario(uid).subscribe({
-      next: (inscripciones) => {
-        this.eventoService.getEventos().subscribe({
-          next: (eventos) => {
-            const ids = inscripciones.map(i => i.$id_evento);
-           // this.agendados = eventos.filter(e => ids.includes(e.id_evento));
-          },
-          error: (err) => console.error('Error cargando eventos para agendados', err)
-        });
-      },
-      error: (err) => console.error('Error cargando inscripciones', err)
-    });
+  cerrarModalEliminar() {
+    this.inscripcionEliminar = null;
+    this.mostrarModalEliminar = false;
   }
 
+ cargarMisInscriciones(): void {
+  const uid = this.auth.usuarioLogueadoId();
+  console.log('Usuario logueado ID:', uid);
+  if (!uid) return;
+
+  this.inscripciones$ = this.inscripcionService.getInscripcionesByUsuario(uid);
+  this.inscripciones$.subscribe({
+    next: data => console.log('Inscripciones recibidas:', data),
+    error: err => console.error('Error en inscripciones$', err)
+  });
+}
 
   agregar(evento: ClassEvento): void {
     const uid = this.auth.usuarioLogueadoId();
     if (!uid) return;
+    this.inscripcionService.registrarInscripcion(evento,uid);
+   }
 
-    //if (this.agendados.some(e => e.id_evento === evento.$id_evento)) return;
-
-    //const nueva = new Inscripcion(uid,evento.id_evento,new Date().toISOString().split('T')[0],1)
-
-
-    this.inscripcionService.registrarInscripcion(evento);
-   /*  addInscripcion(nueva).subscribe({
-      next: () => this.cargarAgendados(),
-      error: (err) => console.error('Error agregando inscripcion', err)
-    });
-  } */
-
-  }
+eliminarInscripcionConfirmada(){
+   if (!this.inscripcionEliminar) return;
+   this.eliminar(this.inscripcionEliminar)
+}
 
 
-  eliminar(evento: Evento): void {
-    const uid = this.auth.usuarioLogueadoId();
-    if (!uid) return;
+ private eliminar(inscrip: Inscripcion): void {
+  this.cerrarModalEliminar()
+  const uid = this.auth.usuarioLogueadoId();
+  if (!uid) return;
 
-    this.inscripcionService.getInscripcionesByUsuario(uid).subscribe({
-      next: (inscripciones) => {
-        const insc = inscripciones.find(i => i.$id_evento === evento.id_evento);
-        if (!insc?.id) {
-          console.warn('No se encontró inscripción para eliminar', insc);
-          return;
-        }
-        this.inscripcionService.deleteInscripcion(insc.id).subscribe({
-          next: () => this.cargarAgendados(),
-          error: (err) => console.error('Error eliminando inscripcion', err)
-        });
-      },
-      error: (err) => console.error('Error buscando inscripciones antes de eliminar', err)
-    });
-  }
+  this.inscripcionService.getInscripcionesByUsuario(uid).subscribe({
+    next: (inscripciones) => {
+      const insc = inscripciones.find(i => i.id === inscrip.id);
+      if (!insc?.id) {
+        console.warn('No se encontró inscripción para eliminar', insc);
+        return;
+      }
+
+      this.inscripcionService.deleteInscripcion(insc.id).subscribe({
+        next: () => {
+           this.mensajeAlert.mensajeEliminacionInscripcion();
+          this.cargarMisInscriciones();
+        },
+        error: (err) => console.error('Error eliminando inscripción', err)
+      });
+    },
+    error: (err) => console.error('Error buscando inscripciones antes de eliminar', err)
+  });
+}
 
 
   imagenEvento(img?: string): string {
-    return img && img.trim() ? img : '/assets/images/default.jpg';
+    return img && img.trim() ? img : '/assets/images/sin.jpg';
   }
+
+ trackByIndex(index: number, item: Inscripcion): any {
+  return item.id ?? index;
+}
+
+abrirModalEliminar(inscripcion: Inscripcion) {
+    this.inscripcionEliminar = inscripcion;
+    this.mostrarModalEliminar = true;
+  }
+
 }
