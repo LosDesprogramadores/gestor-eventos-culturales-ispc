144541d3cbed3f6455eb1915e99edec3a2c7e246
Julio Martin <faria@DESKTOP-TLUUL68>
2025-10-12 16:51:02 -0300

    Add : agregando funcionalidades

--- frontend/gec/src/app/view/home/evento/evento.ts
+++ frontend/gec/src/app/view/home/evento/evento.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Banner } from '../banner/banner';
 import { SEvento } from '../../../services/service-evento/s-evento';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { ClassEvento } from '../../../model/evento';
+import { Auth } from '../../../services/service-autenticacion/auth.service';
 
 @Component({
   selector: 'app-evento',
@@ -14,11 +15,19 @@ import { ClassEvento } from '../../../model/evento';
 })
 export class Evento implements OnInit {
    eventos$ : Observable<ClassEvento[]>;
-    constructor(private serviceEvento : SEvento){
+    constructor(private serviceEvento : SEvento, private router : Router, private aut : Auth){
       this.eventos$ = this.serviceEvento.obtenerEventos();
     }
     
      ngOnInit(): void {
+   }
+
+   existeUsuario(){
+
+    if(this.aut.role == 'ANON'){
+      this.router.navigateByUrl("/inicio-sesion");
+    }
+
    }
 
 
