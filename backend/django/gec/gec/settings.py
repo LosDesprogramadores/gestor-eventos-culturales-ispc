4d3ea65baf662f90bb72fe7e7590dfd1925e4899
Marcelo Portillo <mportillo79@gmail.com>
2025-10-15 21:40:03 -0300

    feat:Se agrega importaciones y configuracion para conecta a la db de render

--- backend/django/gec/gec/settings.py
+++ backend/django/gec/gec/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,12 +95,17 @@ WSGI_APPLICATION = 'gec.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    # DB SQLITE PARA LOCAL
 
+    #   'default': {
+    #       'ENGINE': 'django.db.backends.sqlite3',
+    #       'NAME': BASE_DIR / 'db.sqlite3',
+    #   }
+
+    # DB RENDER requiere .env
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -144,8 +154,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    
+
 }
-
-
-
